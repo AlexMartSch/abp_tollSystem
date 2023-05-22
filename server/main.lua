9cefff2a375e80b877a77da6f5ba33a4abbb813c
Alejandro Martínez <alejandromartpv@icloud.com>
2023-05-22 15:20:59 -0400

    fix: Event name error

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,10 @@
-RegisterNetEvet('abp_TollSystem::Payment', function(tollId) 
+RegisterNetEvent('abp_TollSystem::Payment', function(tollId) 
     local src = source
     local tollData = Config.Tolls[tollId]
     MakePayment(source, tollData.price)
 end)
 
-RegisterNetEvet('abp_TollSystem::PaymentFault', function(tollId) 
+RegisterNetEvent('abp_TollSystem::PaymentFault', function(tollId) 
     local src = source
     local tollData = Config.Tolls[tollId]
     MakePaymentFault(source, tollData.fine)
